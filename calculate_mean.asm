e39cf33bcc273fe7779d34b7eaaf2a160e21c5f3
Daniyal Marofi <daniyaleight8@gmail.com>
2020-06-06 21:15:32 +0430

    used LDR and fixed the counting bug

--- calculate_mean.asm
+++ calculate_mean.asm
@@ -1,12 +1,12 @@
 .ORIG x3000
-        LD R1, arr ; this is the address first value of the array
+        LD R1, arr ; this is the pointer to the first element of the array
         AND R2, R2, #0 ; now R2 is zero for the sum
         LD R4, count ; R4 is the counter
+	    ADD R4, R4, #-1 ; R4=R4-1; because the array is from x1000 to x1063 (0 to 99)
 
-Loop    LDI R3, arr ; R3 is the value of arr
+Loop    LDR R3, R1, #0 ; R3 is the value of arr
         ADD R2,R3,R2 ; R2=R1+R2
-        ADD R1,R1,#1 ; go to the next
-        ST R1, arr ; save next place to arr
+        ADD R1,R1,#1 ; go to the next element
 	    ADD R4, R4, #-1 ; R4=R4-1
 	    BRp Loop
 
@@ -15,8 +15,7 @@ Loop    LDI R3, arr ; R3 is the value of arr
 
         HALT
 
-DIV     LD R4, count ; R4 is the counter
-        AND R5, R5, #0 ; now R2 is zero for the division
+DIV     AND R5, R5, #0 ; now R2 is zero for the division
         LD R6, mcount ; R6 is -100
         ADD R2, R2, R6 ; R2=R2-100
 DIVL    ADD R5,R5,#1 ; R5=R5+1
